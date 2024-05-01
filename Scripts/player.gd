a5294183dddc160f3fee842684eb3664841ff84d
Will Metzler <metzlerwill@gmail.com>
2024-05-01 17:36:02 -0400

    day 1

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,9 +4,9 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 600
+	velocity = direction * 170
 	move_and_slide()
-	print(velocity)
+
 	if velocity.length() == 0:
 		animated_sprite_2d.play("idle")
 	elif velocity.x > 0:
