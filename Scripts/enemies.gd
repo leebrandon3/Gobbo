ce6d91e6db4bf70b45df0b43bf8a54956907fdc3
Victor <victorac2324@gmail.com>
2024-05-02 14:24:07 -0400

    modified hit box and layers

--- Scripts/enemies.gd
+++ Scripts/enemies.gd
@@ -4,15 +4,20 @@ extends CharacterBody2D
 
 
 @onready var player = get_node('/root/World/Gobbo')
-
+#plays the "walk animation"
+func play_walk_animation():
+	animated_sprite_2d.play("walk")
+	
+#makes Kinght Enemy target and walk towards player "Gobbo"
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 60.0
 	move_and_slide()	
+	play_walk_animation()
+	
 	#
-	#
-#func play_walk_animation():
-	#animated_sprite_2d.play("walk")
+	
+
 	##
 #const SPEED = 60 
 #func _process(delta):
