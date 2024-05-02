2f816e83736bc39e801d9b9b6c46a44903f1743b
Victor <victorac2324@gmail.com>
2024-05-02 12:33:57 -0400

    implented enemies targeting player"

--- Scripts/enemies.gd
+++ Scripts/enemies.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-func play_walk_animation():
-	animated_sprite_2d.play("walk")
-	
-const SPEED = 60 
-func _process(delta):
-	play_walk_animation()
-	position.x+=SPEED * delta 
-	##position.y+=1
+
+
+@onready var player = get_node('/root/World/Gobbo')
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 60.0
+	move_and_slide()	
+	#
+	#
+#func play_walk_animation():
+	#animated_sprite_2d.play("walk")
+	##
+#const SPEED = 60 
+#func _process(delta):
+	#play_walk_animation()
+	#position.x+=SPEED * delta 
+	###position.y+=1
