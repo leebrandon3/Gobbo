4ebb083ee8eb4b3ede8d8fa186510e1832c94f41
Stephin Roberts <stephin.roberts@gmail.com>
2024-05-02 15:19:42 -0400

    updated enemies 2

--- Scripts/Enemies2.gd
+++ Scripts/Enemies2.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
+
+
+
+	
+#const SPEED = 60
+#func _process(delta):
+	#play_walk_animation()
+	#position.x+=SPEED * delta
+	
+@onready var player = get_node("/root/World/Gobbo")
 func play_walk_animation():
 	animated_sprite_2d.play("walk")
-	
-const SPEED = 60
-func _process(delta):
-	play_walk_animation()
-	position.x+=SPEED * delta
-	
 
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 90.0
+	move_and_slide()
+	play_walk_animation()
