5badc59b6882b3da164f9c10e40f5489d2bf22ad
Stephin Roberts <stephin.roberts@gmail.com>
2024-05-06 15:18:04 -0400

    updated Enemies 2

--- Scripts/Enemies2.gd
+++ Scripts/Enemies2.gd
@@ -1,27 +1,38 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
-
-var health = 1
-
-#const SPEED = 60
-#func _process(delta):
-	#play_walk_animation()
-	#position.x+=SPEED * delta
 	
 @onready var player = get_node("/root/World/Gobbo")
+
+var health = 2.0
+var direction
+var is_dead = false
+
+var card_drop = preload("res://Scenes/Enemies/card_drop.tscn").instantiate()
+
 func play_walk_animation():
 	animated_sprite_2d.play("walk")
 func play_death_animation():
 	animated_sprite_2d.play("death")
 
 func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 90.0
-	move_and_slide()
-	play_walk_animation()
+	if is_dead == false:	
+		direction = global_position.direction_to(player.global_position)
+		velocity = direction * 90.0
+		move_and_slide()
+		play_walk_animation()
+	elif is_dead == true:
+		velocity = direction * 0.0
+	if health == 0: 
+		play_death_animation()
+		is_dead = true
 	
 func take_damage():
-	health -=1
-	if health == 0 :
-		play_death_animation()
+	health -= 1
+	card_drop.globala_position = global_position
+	add_sibling(card_drop)
+		
+func _on_animated_sprite_2d_animation_finished():
+	print("finished")
+	if is_dead == true:
 		queue_free()
+		print("died")
